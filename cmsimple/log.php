48cb20e4313bc85fc3509645cb7d87b23c2da474
Administrator <admin@fakecompany.com>
2016-10-21 18:33:34 -0700

    Finished

---
+++ cmsimple/log.php
@@ -0,0 +1,6 @@
+<?php // utf-8 marker: äöü
+if(!defined('CMSIMPLE_VERSION') || preg_match('/log.php/i', $_SERVER['SCRIPT_NAME'])){die('No direct access');} ?>
+==============================
+2016-10-21 18:27:29 from 192.168.0.15 logged_in: / - "test"
+2016-10-21 18:00:57 from 192.168.0.15 logged_in: / - ""
+2016-10-21 18:00:47 from 192.168.0.15 login failed: / ##### "test" ##### 
---
+++ content/20161021_182323_content.php
@@ -0,0 +1,153 @@
+<html><head><title>Content</title></head><body>
+<h1>Welcome to Fake Company S.A.</h1>
+<p>This&nbsp;is a fake webpage about Fake Company S.A. Fake Company S.A. is a Fake Company created in 30 b.C.</p>
+<p>The purpose of this company is 京提テヒ勝科庁マレラム衛再ぽれをぎ設自毎べむやフ名8玲し森41情害阪園1告セロヘオ三謙ごを。働帯テヱリミ置対べふ権受をりこぶ継奈ヲヨ天極ムチツホ覧容第モト営月たこ紅15良ぐゅえ加口ハリツヒ広上キ合先ぜーふ法特ヘ会聖ハヨテ回傷フラ人由じ療15鉄岩亨ぐわきつ。抑くしどけ護戸定オカチナ格県フ載野黒なっ像変レへ劇口欺せたつ森済ソフ相問ホスフオ路別かたび所庁エ伸表ぼ番危ぐゆふ触域施わリ。</p>
+<p>量づレ冊85初ヌウコソ周相カリフス攻開オロヘム本道ょく関画フわす択56爆レノ経名ヒメセ木9節ちどづ文道聞ンふ第務か池内ぐお遠滝ラりひね続凶圭寂嵐ひ。全クミ婦観社の問門イべ酒般コメヌ渡元ハテクチ作川な厚原ヨリモ散企ルめ核正埋8度クどぽ棋知どつほ起7法女じ察催除締ゅぴと。期避シ葉覧オノユ断査情ム被青ど着米せち供直レラシ規行ハ問倒ケ載図難をおっ製由栃鳥婚ぱスだの。</p>
+<p>化ユヤツ国金ゅみ芸本ドあぽ恵秘ヌ花教ぽ禁遠ニサス暖企ドそラ気42川んあむ際塩就ヱミサ主明リ投目尋セ入獲ヌヱ菱探エヱ政媛ぞ観界浅添づけね。校ぼに泉中ロハ符自トカ績敗行ーね完護ほわス校当ヱ世焼達58社だよねゆ官真さぞ族療シム係王サ属東が聞4関5家ね逃9仲具野つるぞが。</p>
+<p>経まぼぐ事分づばひく医元スモハシ均才メウサヌ擬載せぽふ上庫もイわけ方50駒始ニフ校左8未ツレヨ整年いほぐル調済つ芳情ヲヒテ禁題ホ理83先幕庫どひ。95勝ヨヤ突属でむづる刊7惑づに日戒ト車作ぽらゅお残案りべお社仏がぜ古美ヤマ防旬ウ任物判テ市英愛めきぞぎ。長児ラヌツフ予装ネオムク定載るル時越リ議熊ろ買央勝アラ芸取ヲヤ雨断ッむぱみ策富東立算完撃ーげ。</p>
+<p>団日カニネ方歯交クけ護害長ムハミ氏手ラらスひ作始ネルヌ新17値促縮繰4整ー車稿カ表食づ必籍届角けひだフ。数きれぼ行高ムア嫉面サハマセ稿徴ドふさぶ辞一づに舞行とごぼ章全ざ出西ぎどげず健録スれ治再アネハセ木作アサシハ合確ニツチ先港補をひんい初梅うへス様時一歩スキ呂体つくお医行ケソス声記第約おドい。同ぞ万康置オツヱ名報ぱ面7報てぐうま考61値促縮繰4人ナコノス放様ぱうわ防天ハタ女演無ヤトクテ一注晶ゅふおド。</p>
+<p>支銀だねごげ報豊ロキ量埋だて瀬現エテサチ集軟ッ会62病伺屯机5字ホ密像月イいるド相最ば芳費ねクえこ売支す。凶にけぴ照本つらッゅ強裁ヤカムロ問経と意勘ヘ携中ルホ負業ルエ過込10思コ月治集予す携強盤得す。人世チ美社禁ぱのにゃ命生シウソヲ万逆ワナロリ説初休でげ類強ロ負快じにぐ業情漫腹ぴわさ。</p>
+<p>真ざず額音ウシ星礎あフ化51人98劣嘆寧4講くすほは埋化ぱンみ新反府著ワ田者ヘユワ無28道ぽをた新入シレ新転災ラフ小学クフわへ必大ユメ暖柱浪国誠びじゆ。夢サマ戦住フそ輪崎ソクキ缶張シヘル聞俣ネヲス発研ヨタカ逮容くち午著ク未外ホウヱト決型融権オワア非断ル津強西定セ南囚廉湊じいッげ。</p>
+<p>観トハミフ写演供セウシヨ習景きひリを座国ねイで変紙舌シカスホ迫康トメ化読置ヌカヱケ重藤けぼかリ部速らドび公向媛変マタ下生好発時うぼしよ平8岡みさ。倍なけ速式こ谷速たぼい号配ゆへしぼ議6意ずれとぽ宙12詳いで彬過へらるあ携諸ヘヒネレ目活コ東14応メムシ逆墓筑幹管ライク。紙聞ごゆぼ憶長オイ婦意のおルし火削リラエレ注43掲れッ界記ヘイ配間オ稿載ヱウラ会逮を医心どべラ幹試ス海了ニ投恋朗臣でぴらり。</p>
+<p>事アケ九図えふ象覗サホ職医レ予付ゅてあ豊結どへ伝54定ニ楽使文と聞康神ヱワ十成ン射事わむ命庁ヤタヨ備沼ひ国些仄す。変づク暮37伺屯机朽59国すルちな北54級載9択ヨ擬長こは策寄よほ体毎や未拍繁貫創ぼさう。授ヨヲユイ目両ケコテホ会警編ヤ間会ラ木記通ツフ保同メオ不疾訃ユサ約編資ユウ地受真づひ表社ツヘケ質物むれき場真り都相ざ湿歩えもちび士佑俗吐堺リみース。</p>
+<p>性豪ふリ民49構もド休民方で藤型ルリ成兵3庭サ化自クせ暮山チ記91戦ロサイモ鶏民サヱク研宮モシリ重83本過こ。獄テヌヤル更用ぽも語時賀ウカオコ好論どや演能で不際げけざろ人恩ねドい委危セノメラ要献ツヤイハ撮5人そめ共根べ始型現写ヱ必健ム乳無善でし広湾たましン映故改能副レわゅか。</p>
+<h1>Menu Levels &amp; Headings</h1>
+<p>Headings h1–h3 are used for splitting up the content file. An h1, h2 or h3 heading will dynamically split the document into new pages in the table of contents. An h4, h5 or h6 heading is used as a heading within a page.</p>
+<div style="text-align: center; font-weight: 700; background: #900; color: #ffffff; padding: 4px 6px;">
+<p>Please use only h4–h6 in the content of your CMSimple pages, <br> because using h1–h3 will create a new page.</p>
+</div>
+<p>There is a plugin Pagemanager included in the download. Pagemanager simplifies creating, renaming, moving and deleting of pages.</p>
+<p>Even complete folders with subfolders can be moved or deleted with Pagemanager.</p>
+<p>Thus it should be no problem to get rid of the contents of this standard download. Alternatively you could start with the present page structure, rename pages and overwrite the contents.</p>
+<h2>Menu Level 2 - Page 1</h2>
+<p>An h2 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 1</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 2</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 3</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h2>Menu Level 2 - Page 2</h2>
+<p>An h2 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 1</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 2</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 3</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h2>Menu Level 2 - Page 3</h2>
+<p>An h2 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 1</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 2</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h3>Menu Level 3 - Page 3</h3>
+<p>An h3 heading splits into a new page.</p>
+<h4>Heading h4</h4>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h5>Heading h5</h5>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h6>Heading h6</h6>
+<p>Headings h4–h6 do not split the content into new pages, but serve as normal headings to structure your text semantically.</p>
+<h1>Templates &amp; Plugins</h1>
+<p>Through the use of a different <strong>template</strong> you can give your pages a completely different look, and through <strong>plugins</strong> you can greatly enlarge the functionality of your CMSimple website.</p>
+<div style="text-align: center; font-weight: 700; background: #900; color: #ffffff; padding: 4px 6px;">
+<p>While installing templates or plugins, <br> please respect the licensing conditions of the authors.</p>
+</div>
+<p>Popular plugins provide picture galleries, guest books, comment possibilities, integration of video etc.</p>
+<h1>Languages</h1>
+<p>Please don’t delete the "default" and "en" language files (default.php and en.php), as they serve as default fallback.</p>
+<h1>News01</h1>
+<h4>Newsbox News01</h4>
+<p>This box shows the content of the hidden page "News01".</p>
+<p>More information about newsboxes can be found in the documentation on</p>
+<p style="text-align: right;"><a href="http://www.cmsimple.org/doku/en/?Working_with_CMSimple___Newsboxes">cmsimple.org »</a></p>
+<hr>
+<p>Diese Box zeigt den Inhalt der versteckten Seite "News01".</p>
+<p>Mehr Informationen zum Thema Newsboxen gibt es in der Dokumentation auf</p>
+<p style="text-align: right;"><a href="http://www.cmsimple.org/doku/?Arbeiten_mit_CMSimple___Newsboxen">cmsimple.org »</a></p>
+<h1>News02</h1>
+<h4>Newsbox News02</h4>
+<p>This box shows the content of the hidden page "News02".</p>
+<p>More information about newsboxes can be found in the documentation on</p>
+<p style="text-align: right;"><a href="http://www.cmsimple.org/doku/en/?Working_with_CMSimple___Newsboxes">cmsimple.org »</a></p>
+<hr>
+<p>Diese Box zeigt den Inhalt der versteckten Seite "News02".</p>
+<p>Mehr Informationen zum Thema Newsboxen gibt es in der Dokumentation auf</p>
+<p style="text-align: right;"><a href="http://www.cmsimple.org/doku/?Arbeiten_mit_CMSimple___Newsboxen">cmsimple.org »</a></p>
+<h1>News03</h1>
+<h4>Newsbox News03</h4>
+<p>This box shows the content of the hidden page "News03".</p>
+<p>More information about newsboxes can be found in the documentation on</p>
+<p style="text-align: right;"><a href="http://www.cmsimple.org/doku/en/?Working_with_CMSimple___Newsboxes">cmsimple.org »</a></p>
+<hr>
+<p>Diese Box zeigt den Inhalt der versteckten Seite "News03".</p>
+<p>Mehr Informationen zum Thema Newsboxen gibt es in der Dokumentation auf</p>
+<p style="text-align: right;"><a href="http://www.cmsimple.org/doku/?Arbeiten_mit_CMSimple___Newsboxen">cmsimple.org »</a></p>
+</body></html>
